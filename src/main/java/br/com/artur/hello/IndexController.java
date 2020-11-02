69d80d3ff669114fae2b530fb03c0c5aae484ead
artur <artur@artur.com>
2020-11-02 02:08:00 -0300

    novo metodo

--- src/main/java/br/com/artur/hello/IndexController.java
+++ src/main/java/br/com/artur/hello/IndexController.java
@@ -1,14 +1,21 @@
 package br.com.artur.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+
     @GetMapping( "/" )
     public String hello() {
         return "hello world!";
+    }
+
+    @GetMapping( "/{name}" )
+    public String hello(@PathVariable String name) {
+        return "hello " + name + "!";
     }
 
     @GetMapping( "/teste" )
